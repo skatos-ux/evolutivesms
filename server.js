89925d8960974d0551c62ae968564bf95b559141
skatos-ux <nicolas.magne1@outlook.fr>
2019-06-22 19:55:50 +0200

    Web Version

--- server.js
+++ server.js
@@ -2,24 +2,34 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/EvolutiveSMSModel'), //created model loading here
+  SendAPI = require('./api/models/EvolutiveSMSModel'), //created model loading here
+  SendWEB = require('./web/models/EvolutiveSMSModel'),
   bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Tododb', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/Senddb', {useNewUrlParser: true});
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/web/views');
 
-var routes = require('./api/routes/EvolutiveSMSRoutes'); //importing route
-routes(app); //register the route
+app.get('/', function (req, res) {
+  res.send('index.ejs');
+});
+
+var APIroutes = require('./api/routes/EvolutiveSMSRoutes'); //importing route
+var WEBroutes = require('./web/routes/EvolutiveSMSRoutes');
+APIroutes(app); //register the route
+WEBroutes(app);
+
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
+
 
 
 app.listen(port);
